e1a8a6ecbfd21e093cffe09cfc5e6a9cd249d848
Max-Scholten <PS229429@summacollege.nl>
2023-10-03 21:27:15 +0200

    L2 Op1 Name change in the new models

--- app/Http/Controllers/BandController.php
+++ app/Http/Controllers/BandController.php
@@ -3,7 +3,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\BandRequest;
 use Illuminate\Http\Request;
-use App\Models\Bands;
+use App\Models\Band;
 
 class BandController extends Controller
 {
